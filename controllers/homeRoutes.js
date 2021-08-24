86bc44c8cf1eeb59d1a6a26737aef3f9549084fc
Scott T <scott.tessm@gmail.com>
2021-08-24 16:54:12 -0500

    Continued work on getting homepage handlebars and css going.

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+
+
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login', {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/addpet', async (req, res) => {
+  try {
+    res.render('createPetPage', {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
